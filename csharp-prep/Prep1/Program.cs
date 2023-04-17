3a474a57438dd451bf55981cfab29af8068c0de0
Johnson <parkerwj@byui.edu>
2023-04-17 15:50:26 -0600

    Added a comment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,12 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Ask the user for their first and last name.
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
-        
+
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
 
     }
