e3e7af03a573df6bef5456b2cb95581a1da9cc9a
Johnson <parkerwj@byui.edu>
2023-04-18 11:06:58 -0600

    Created the magic number game with loops.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+
+        do
+        {
+            // Generate a random number and ask the user to guess the number.
+            // Provide feedback if the number is higher or lower. 
+            // Loop the program while replay is "y".
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+            int userGuess = 0;
+            int numberGuesses = 0;
+            Console.WriteLine($"Guess the magic number? ");
+            while (magicNumber != userGuess)
+            {
+                Console.Write("What is your guess? ");
+                string userString = Console.ReadLine();
+                userGuess = int.Parse(userString);
+                if (magicNumber > userGuess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < userGuess)
+                {
+                    Console.WriteLine("Lower");
+                }
+            numberGuesses ++;    
+            }
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {numberGuesses} guesses.");
+            Console.WriteLine("Do you want to play again? (y/n)");
+            response = Console.ReadLine();
+        } while (response == "y"); 
+    Console.WriteLine("Thanks for playing!");
     }
 }
