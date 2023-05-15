898392bb417366dc0a73a782bbb106c6c684d9a8
Johnson <parkerwj@byui.edu>
2023-05-15 13:10:00 -0600

    Finished the GetArea program for all shapes.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+            Circle c1 = new Circle("Blue", 3);
+            shapes.Add(c1);
+
+            Rectangle r1 = new Rectangle("Green", 4, 5);
+            shapes.Add(r1);
+
+            Square s1 = new Square("Yellow", 6);
+            shapes.Add(s1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
